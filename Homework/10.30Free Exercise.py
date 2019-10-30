51e057e51d83367998ec5ca038c1dbaa53db6a41
BPerez2021 <55213093+BPerez2021@users.noreply.github.com>
2019-10-30 16:56:02 -0400

    Create 10.30Free Exercise.py

---
+++ Homework/10.30Free Exercise.py
@@ -0,0 +1,18 @@
+#Brieana Perez
+#Attempting 7-2 Conversions
+
+#%%
+# 1 ft = 12 in
+def length(ft):
+    return (ft)*12
+
+#%%
+#Building upon the past free exercise
+#1 week = 7 days
+def weeks(day):
+    return (day)/7
+
+#%%
+#1 day = 24 Hours
+def day(hours):
+    return (hours)/24
