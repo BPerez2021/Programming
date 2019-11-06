b0057a4662cf1b0072b93c069f874a81d83f212c
BPerez2021 <55213093+BPerez2021@users.noreply.github.com>
2019-11-06 18:52:39 -0500

    Create Reflection 11.6.py

---
+++ Homework/Reflection 11.6.py
@@ -0,0 +1,54 @@
+##Week 3 reflection/ Reflection 11.6 found under "#Discount"
+
+#%%
+4 + 4.5 + 0.375
+
+#%%
+1500 * 0.08875
+
+#%%
+995.50 * 0.08875
+
+#%%
+def tax(subtotal):
+    return subtotal * 0.08875
+
+#%%
+tax(1500)
+
+#%%
+1500 + tax(1500)
+
+#%%
+200 + 1500 + tax(1500)
+
+#%%
+sum = 1500
+sum = sum + sum * 0.08875 # Add the tax amount
+
+#%%
+def tax(subtotal):
+    return subtotal + subtotal * 0.08875
+
+# %%
+##Discount
+def Discount(subtotal):
+    return subtotal * .20
+##amount off
+
+# %%
+#amount have to pay
+def Discount(subtotal):
+    return subtotal - subtotal * .20 
+
+#%%
+
+
+
+
+
+
+
+
+
+# %%
